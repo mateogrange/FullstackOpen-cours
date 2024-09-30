6e5f86feea4594ddbaafe3c3bb1e7f64befdce55
mateo <grange.mateo45@gmail.com>
2024-09-30 11:00:42 +0200

    add exercises 1.5 : courseinfo, step 5

--- part1/courseinfo/src/App.tsx
+++ part1/courseinfo/src/App.tsx
@@ -2,7 +2,7 @@
 const Header = (props) => {
     return (
         <div>
-            <h1>{props.course}</h1>
+            <h1>{props.course.name}</h1>
         </div>
     )
 }
@@ -34,28 +34,30 @@ const Total = (props) => {
 }
 
 const App = () => {
-    const course = 'Half Stack application development'
-    const parts = [
-        {
-            name: 'Fundamentals of React',
-            exercises: 10
-        },
-        {
-            name: 'Using props to pass data',
-            exercises: 7
-        },
-        {
-            name: 'State of a component',
-            exercises: 14
-        }
-    ]
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
     return (
         <div>
             <Header course={course}/>
-            <Content parts={parts[0]} />
-            <Content parts={parts[1]} />
-            <Content parts={parts[2]} />
-            <Total parts={parts} />
+            <Content parts={course.parts[0]} />
+            <Content parts={course.parts[1]} />
+            <Content parts={course.parts[2]} />
+            <Total parts={course.parts} />
         </div>
     )
 }
