27ce9119d781b1d0d63774f9893ee8c4d10090cf
mateo <grange.mateo45@gmail.com>
2024-10-01 11:33:20 +0200

    add unicafe exercise

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,131 +1,156 @@
 import { useState } from 'react'
 
 const Header = (props) => {
-    return (
-        <div>
-            <h1>{props.course.name}</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>{props.course.name}</h1>
+    </div>
+  )
 }
 
 const Content = (props) => {
-    return (
-        <div>
-            <p>{props.parts.name} {props.parts.exercises}</p>
-        </div>
-    )
+  return (
+    <div>
+      <p>{props.parts.name} {props.parts.exercises}</p>
+    </div>
+  )
 }
 
 const Total = (props) => {
-    let total = 0
+  let total = 0
 
-    props.parts.forEach(value => {
-        total = total + value.exercises
-    })
+  props.parts.forEach(value => {
+    total = total + value.exercises
+  })
 
-    return (
-        <div>
-            <p>{total}</p>
-        </div>
-    )
+  return (
+    <div>
+      <p>{total}</p>
+    </div>
+  )
 }
 
 const Display = ({counter}) => {
-    return (
-        <div>{counter}</div>
-    )
+  return (
+    <div>{counter}</div>
+  )
 }
 
 const Button = ({onClick, text}) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
     return (
-        <button onClick={onClick}>
-            {text}
-        </button>
+      <div>
+        the app is used by pressing the buttons
+      </div>
     )
+  }
+  return (
+    <div>
+      button press history : {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const ButtonHistory = ({onClick, text, click}) => {
+  return (
+    <div>
+      <button onClick={onClick}>
+        {text}
+      </button>
+      {click}
+    </div>
+  )
 }
 
 const App = () => {
-    //création de class en js
-    class Person {
-        constructor(name, age) {
-            this.name = name
-            this.age = age
-        }
-        greet() {
-            console.log('hello my name is ' + this.name + ', I am ' + this.age)
-        }
+  //création de class en js
+  class Person {
+    constructor(name, age) {
+      this.name = name
+      this.age = age
     }
-    const adam = new Person('adam lucien', 24)
-    const john = new Person('john doe', 26)
-    // adam.greet()
-    // john.greet()
-
-    //création useState
-    const [counter, setCounter] = useState(0)
-    const increaseByOne = () => setCounter(counter + 1)
-    const decreaseByOne = () => setCounter(counter - 1)
-    const setToZero = () => setCounter(0)
-
-    const course = {
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10
-            },
-            {
-                name: 'Using props to pass data',
-                exercises: 7
-            },
-            {
-                name: 'State of a component',
-                exercises: 14
-            }
-        ]
+    greet() {
+      console.log('hello my name is ' + this.name + ', I am ' + this.age)
     }
+  }
+  const adam = new Person('adam lucien', 24)
+  const john = new Person('john doe', 26)
+  // adam.greet()
+  // john.greet()
 
-    const [left, setLeft] = useState(0)
-    const [right, setRight] = useState(0)
-    const [allClicks, setAll] = useState([])
+  //création useState
+  const [counter, setCounter] = useState(0)
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+  const setToValue = (newValue) => {
+    setCounter(newValue)
+  }
 
-    const handleLeftClick = () => {
-        setAll(allClicks.concat('L'))
-        setLeft(left + 1)
-    }
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
-    const handleRightClick = () => {
-        setAll(allClicks.concat('R'))
-        setRight(right + 1)
-    }
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
-    return (
-        <div>
-            <Header course={course}/>
-            <Content parts={course.parts[0]}/>
-            <Content parts={course.parts[1]}/>
-            <Content parts={course.parts[2]}/>
-            <Total parts={course.parts}/>
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
 
-            <Display counter={counter}/>
-            <Button onClick={increaseByOne} text={'plus'}/>
-            <Button onClick={setToZero} text={'zero'}/>
-            <Button onClick={decreaseByOne} text={'minus'}/>
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
 
-            <div>
-                {left}
-                <button onClick={handleLeftClick}>
-                    left
-                </button>
-                <button onClick={handleRightClick}>
-                    right
-                </button>
-                {right}
-                <p>
-                    {allClicks.join(' ')}
-                </p>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <Header course={course}/>
+      <Content parts={course.parts[0]}/>
+      <Content parts={course.parts[1]}/>
+      <Content parts={course.parts[2]}/>
+      <Total parts={course.parts}/>
+
+      <Display counter={counter}/>
+      <Button onClick={increaseByOne} text={'plus'}/>
+      <Button onClick={setToZero} text={'zero'}/>
+      <Button onClick={decreaseByOne} text={'minus'}/>
+      <button onClick={() => (setToValue(1000))}>
+        thousand
+      </button>
+
+      <div>
+        <ButtonHistory onClick={handleLeftClick} text={"left"} click={left}/>
+        <ButtonHistory onClick={handleRightClick} text={"right"} click={right}/>
+        <History allClicks={allClicks}/>
+      </div>
+
+    </div>
+  )
 }
 
 export default App
---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,91 @@
+import { useState } from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Give feedback</h1>
+    </div>
+  )
+}
+
+const Button = ({handleClick, text}) => {
+  return (
+    <div>
+      <button onClick={handleClick}>{text}</button>
+    </div>
+  )
+}
+
+const Table = ({feedBack, text}) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{feedBack}</td>
+    </tr>
+  )
+}
+
+const Stat = ({good, neutral, bad, total}) => {
+  const avg = (good + bad) / 2
+  const percentPositive = (100 * good) / total
+
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <table>
+        <tbody>
+        <Table feedBack={good} text={"good"}/>
+        <Table feedBack={neutral} text={"neutral"}/>
+        <Table feedBack={bad} text={"bad"}/>
+        <tr>
+          <td>average</td>
+          <td>{avg}</td>
+        </tr>
+
+        <tr>
+          <td>Positive</td>
+          <td>{percentPositive}%</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+const App = () => {
+  // enregistrer les clics de chaque bouton dans un état différent
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleClickGood = () => {
+    setGood(good + 1)
+  }
+  const handleClickBad = () => {
+    setBad(bad + 1)
+  }
+  const handleClickNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+  const total = good + bad + neutral
+
+  return (
+    <div>
+      <Header/>
+      <Button handleClick={handleClickGood} text={"good"}/>
+      <Button handleClick={handleClickNeutral} text={"neutral"}/>
+      <Button handleClick={handleClickBad} text={"bad"}/>
+      <h2>Statistics</h2>
+      <Stat good={good} neutral={neutral} bad={bad} total={total}/>
+    </div>
+  )
+}
+
+export default App
