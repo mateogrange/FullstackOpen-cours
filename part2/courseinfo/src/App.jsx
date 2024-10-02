31fea4d81f63053a2e79a4c737381c49178cd64d
mateo <grange.mateo45@gmail.com>
2024-10-02 18:00:11 +0200

    init part2 exo 2.9

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number:'040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const handleChangeName = (event) => {
     setNewName(event.target.value)
@@ -15,16 +19,22 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilter = (event) => {
+    setFilter(event.target.value)
+  }
+
   const addNewName = (event) => {
     event.preventDefault()
     const nameObject = {
       name: newName,
-      number: newNumber
+      number: newNumber,
+      id: persons.length + 1,
     }
     const found = persons.some(el => el.name.toUpperCase() === newName.toUpperCase())
     if(!found){
       setPersons(persons.concat(nameObject))
       setNewName('')
+      setNewNumber('')
     } else {
       alert(newName + ' is already added to phonebook')
     }
@@ -33,11 +43,19 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>filter shown with <input onChange={handleFilter}  value={filter}/></div>
+      </form>
+
+      <h2>Add a new </h2>
       <form onSubmit={addNewName}>
         <div>name: <input onChange={handleChangeName} value={newName}/></div>
         <div>number: <input onChange={handleChangeNumber} value={newNumber}/></div>
-        <div><button type="submit">add</button></div>
+        <div>
+          <button type="submit">add</button>
+        </div>
       </form>
+
       <h2>Numbers</h2>
       {persons.map((person, index) =>
         <li key={index}>{person.name} {person.number}</li>
