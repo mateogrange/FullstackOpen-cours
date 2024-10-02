30fa10aea3156b6f07821481b29fac4c1e648b04
mateo <grange.mateo45@gmail.com>
2024-10-02 16:20:48 +0200

    add part2 exo2.6

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -6,20 +6,36 @@ const App = () => {
   ])
   const [newName, setNewName] = useState('')
 
+  const handleChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+    console.log(persons)
+  }
+
+  const addNewName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName
+    }
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addNewName}>
         <div>
-          name: <input/>
+          name: <input onChange={handleChange} value={newName}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
-      <div>debug: {newName}</div>
+      {persons.map((person, index) =>
+        <li key={index}>{person.name}</li>
+      )}
     </div>
   )
 }
