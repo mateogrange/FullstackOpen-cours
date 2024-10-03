016e1d11ab68a0d0ff759977e4d9679ff19186fb
mateo <grange.mateo45@gmail.com>
2024-10-03 11:10:55 +0200

    fix part2 exo 2.9

--- part2/courseinfo/src/main.tsx
+++ part2/courseinfo/src/main.tsx
@@ -1,25 +1,13 @@
 import ReactDOM from "react-dom/client"
-
+import React from 'react'
 import App from "./App.js"
 
-const notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-]
+const rootElement = document.getElementById("root");
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <App notes={notes} />
-)
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+      <App />
+  );
+} else {
+  console.error('No root element found');
+}
