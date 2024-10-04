069456846eda118382479cbcea199a20ad47652b
mateo <grange.mateo45@gmail.com>
2024-10-04 10:03:35 +0200

    add part2 exo 2.11

--- part2/courseinfo/src/App.tsx
+++ part2/courseinfo/src/App.tsx
@@ -1,72 +1,35 @@
-import {useState, useEffect, ChangeEvent} from 'react'
+import {useState, useEffect} from 'react'
 import axios, {AxiosResponse} from 'axios'
-import Note from './components/Note'
 import React from 'react'
 
-interface Notes {
+interface Persons {
+  name: string;
+  number: string;
   id: number;
-  content: string;
-  date: string;
-  important: boolean;
 }
 
 const App = () => {
-  const [notes, setNotes] = useState<Notes[]>([])
-  const [newNote, setNewNote] = useState('')
-  const [showAll, setShowAll] = useState(false)
+  const [persons, setPersons] = useState<Persons[]>([])
 
   useEffect(() => {
-    console.log('effect')
     axios
-      .get('http://localhost:3001/notes')
+      .get('http://localhost:3001/persons')
       .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
+        console.log(response.data)
+        setPersons(response.data)
       })
-  }, [])
+  }, []);
 
-  console.log('render', notes.length, 'notes')
-
-  const addNote = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    const noteObject = {
-      content: newNote,
-      important: Math.random() > 0.5,
-      date: new Date().toISOString(),
-      id: notes.length + 1,
-    }
-
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
-  }
-
-  const handleNoteChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setNewNote(event.target.value)
-  }
-
-  const notesToShow = showAll
-    ? notes
-    : notes.filter(note => note.important)
+  console.log(persons)
 
   return (
     <div>
-      <h1>Notes</h1>
-      <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all' }
-        </button>
-      </div>
-      <ul>
+      <h1>Persons</h1>
         <ul>
-          {notesToShow.map(note =>
-            <Note key={note.id} note={note} />
+          {persons.map(person =>
+            <li key={person.id}> {person.name} {person.number}</li>
           )}
         </ul>
-      </ul>
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} />
-        <button type="submit">save</button>
-      </form>
     </div>
   )
 }
