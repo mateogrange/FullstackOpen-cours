2c0834b7392be8f39bba339cd4dcda16ec97a385
mateo <grange.mateo45@gmail.com>
2024-10-10 17:36:54 +0200

    add part2 exo 2.20

--- part2/courseinfo/src/components/Countries.tsx
+++ part2/courseinfo/src/components/Countries.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 
 interface Languages {
   [key: string]: string;
+}
+
+interface WeatherData {
+  main: {
+    temp: number;
+  },
+  wind: {
+    speed: number;
+  },
+  weather:[
+    Object:{
+      icon: string;
+    }
+  ]
 }
 
 interface Country {
@@ -21,6 +36,20 @@ interface CountriesProps {
 }
 
 const Countries: React.FC<CountriesProps> = ({countrie}) => {
+  const [weather, setWeather] = useState<WeatherData | null>(null)
+  const [imgUrl, setImgUrl] = useState('')
+  // @ts-ignore
+  const apiKey = import.meta.env.VITE_API_KEY;
+
+  useEffect(() => {
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?q=${countrie.capital}&appid=${apiKey}&units=metric`)
+      .then((response) => {
+        setWeather(response.data)
+        setImgUrl(`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
+      })
+  }, [countrie.capital]);
+
   return (
     <div key={countrie.cca2}>
       <h2>{countrie.name.common}</h2>
@@ -40,8 +69,32 @@ const Countries: React.FC<CountriesProps> = ({countrie}) => {
           </ul>
         )}
       </div>
-      <div style={{fontSize: '100px'}}>
+      <div style={{fontSize: '150px'}}>
         {countrie.flag}
+      </div>
+      <div>
+        <h2>Weather in {countrie.name.common}</h2>
+        {weather && weather.main.temp? (
+            <p>Temperature: {weather.main.temp} Celsius</p>
+
+          ) : (
+            <p>no data</p>
+          )
+        }
+
+        {weather && weather.main.temp? (
+          <img alt={imgUrl} src={imgUrl}/>
+        ) : (
+          <p>no data</p>
+        )
+        }
+
+        {weather && weather.wind.speed? (
+            <p>Wind: {weather.wind.speed} m/s</p>
+          ) : (
+            <p>no data</p>
+          )
+        }
       </div>
     </div>
   )
